948b72d7f474c4458626b94c2fd968e356cf7c81
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2020-12-21 17:18:28 +0600

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import {Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    
+    <div>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+      <Button variant="link">Link</Button>
+
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
+    
   );
 }
 

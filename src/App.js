16d3fe75b753b53946904cb2e1200d5235b9e781
Mahfuzul-Islam55 <43821861+Mahfuzul-Islam55@users.noreply.github.com>
2020-12-27 00:24:02 +0600

    configure routing and installing some packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import {Button} from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { BrowserRouter } from 'react-router-dom';
+import AppRoute from './route/AppRoute';
+
 function App() {
   return ( 
     
-    <div>
-      <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
-      <Button variant="link">Link</Button>
-
-      <FontAwesomeIcon icon={faCoffee} />
-    </div>
+    <Fragment>
+      <BrowserRouter>
+      <AppRoute></AppRoute>
+      
+      </BrowserRouter>
+    </Fragment>
     
   );
 }
